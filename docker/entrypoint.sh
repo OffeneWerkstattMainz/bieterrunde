0f555b5aa3b37e9691053bb72b5a523b424bd6f0
Ulrich Petri <ulo@ulo.pe>
2026-04-15 22:28:51 +0200

    Improve docker entrypoint and compose config

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,7 +2,20 @@
 
 set -euo pipefail
 
-uv run --group prod --no-group dev python manage.py migrate
-uv run --group prod --no-group dev python manage.py collectstatic --noinput
+MODE="${1:-web}"
+shift
 
-uv run --group prod --no-group dev gunicorn "$@"
+case "$MODE" in
+  web)
+    uv run --group prod --no-group dev python manage.py migrate
+    uv run --group prod --no-group dev python manage.py collectstatic --noinput
+
+    uv run --group prod --no-group dev gunicorn "$@"
+    ;;
+  worker)
+    uv run --group prod --no-group dev manage.py rqworker --job-class django_tasks_rq.Job
+  *)
+    echo "Unknown mode: $MODE"
+    exit 1
+    ;;
+esac
